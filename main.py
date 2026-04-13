e683aebf8093ea1b960b321d8d2976edff32607b
mumtozabegimolimova-web <mumtozabegimolimova@gmail.com>
2026-04-13 20:39:55 +0500

    update dashboard

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def prepare_data(df):
     df["timestamp"] = df["timestamp"].astype(str)
 
     # преобразуем в datetime
-    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    df["timestamp"] = pd.to_datetime(df["timestamp"], format="%m/%d/%y %I:%M:%S %p")
 
     # удаляем мусор
     df = df.dropna(subset=["timestamp"])
